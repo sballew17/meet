6754f5b28c8154aaea52e30a66947b6575a75039
sballew17 <samuel.ballew17@gmail.com>
2021-07-16 22:17:04 -0400

    ye

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -87,6 +87,9 @@ module.exports.getAccessToken = async (event) => {
       // Respond with OAuth token 
       return {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
         body: JSON.stringify(token),
       };
     })
@@ -95,9 +98,6 @@ module.exports.getAccessToken = async (event) => {
       console.error(err);
       return {
         statusCode: 500,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
         body: JSON.stringify(err),
       };
     });
