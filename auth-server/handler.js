3a736e9dcc7fd6986d1add03d9dd7f668e9c1ec4
sballew17 <samuel.ballew17@gmail.com>
2021-07-21 22:26:02 -0400

    .

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -11,9 +11,9 @@ const credentials = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://gregoriodelasheras.github.io/meetbot'],
+  redirect_uris: ['https://sballew17.github.io/meet/'],
   javascript_origins: [
-    'https://gregoriodelasheras.github.io',
+    'https://sballew17.github.io',
     'http://localhost:3000',
   ],
 };
