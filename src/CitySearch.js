68084a4bcffb16ff08439f8bffd2470131193e68
sballew17 <samuel.ballew17@gmail.com>
2021-07-28 19:16:05 -0400

    added alert.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
@@ -11,10 +12,19 @@ class CitySearch extends Component {
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({
-            query: value,
-            suggestions,
-        });
+        if (suggestions.length === 0) {
+            this.setState({
+                query: value,
+                infoText: 'We can not find the city you are looking for. Please try another city',
+            });
+        } else {
+            return this.setState({
+                query: value,
+                suggestions: [],
+                showSuggestions: false,
+                infoText: ''
+            });
+        }
     };
     handleItemClicked = (suggestion) => {
         this.setState({
@@ -26,6 +36,7 @@ class CitySearch extends Component {
     render() {
         return (
             <div className="CitySearch">
+                <InfoAlert text={this.state.infoText} />
                 <input
                     type="text"
                     className="city"
