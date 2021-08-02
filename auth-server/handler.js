18a1427d61946b61ad224ec884ddd15c091328d3
sballew17 <samuel.ballew17@gmail.com>
2021-08-02 11:24:19 -0400

    handler

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -53,7 +53,7 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify(token),
       };
